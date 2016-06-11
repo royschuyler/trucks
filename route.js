f0f32075b92b9f57cffeae0313fd6ae950fb58b2
royschuyler <royschuyler@yahoo.com>
2016-06-11 17:44:38 -0500

    conditional form

--- route.js
+++ route.js
@@ -2,233 +2,276 @@ var passport = require('passport');
 var bcrypt = require('bcrypt-nodejs');
 var Model = require('./model');
 
-
-
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'Hollie12123',
-  database : 'doctor'
+  host: 'localhost',
+  user: 'root',
+  password: 'Hollie12123',
+  database: 'doctor'
 });
 
-connection.connect(function(err){
-if(!err) {
+connection.connect(function(err) {
+  if (!err) {
     console.log("Database is connected ... \n\n");
-} else {
+  } else {
     console.log("Error connecting database ... \n\n");
-}
+  }
 });
 
 //-----------------------------------------------------------------------
+var GUIDText = "Current Patient ID: ";
 
+var GUID = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  var r = Math.random() * 16 | 0,
+    v = c == 'x' ? r : (r & 0x3 | 0x8);
+  return v.toString(16);
+});
 
+var GUIDReady = GUIDText + GUID;
 
 //-----------------------------------------------------
 
 var index = function(req, res, next) {
-   if(!req.isAuthenticated()) {
-      res.redirect('/signin');
-   } else {
+  if (!req.isAuthenticated()) {
+    res.redirect('/signin');
+  } else {
 
-      var user = req.user;
+    var user = req.user;
 
-      if(user !== undefined) {
-         user = user.toJSON();
-      }
-      res.render('index', {title: 'Home', user: user});
-   }
+    if (user !== undefined) {
+      user = user.toJSON();
+    }
+    res.render('index', {
+      title: 'Home',
+      user: user
+    });
+  }
 };
 
 //-------------------------------------------------------
 
 var home = function(req, res, next) {
-   if(!req.isAuthenticated()) {
-      res.redirect('/signin');
-   } else {
+  if (!req.isAuthenticated()) {
+    res.redirect('/signin');
+  } else {
 
-      var user = req.user;
+    var user = req.user;
 
-      if(user !== undefined) {
-         user = user.toJSON();
-      }
-      res.render('home', {title: 'Home', user: user});
-   }
+    if (user !== undefined) {
+      user = user.toJSON();
+    }
+    res.render('home', {
+      title: 'Home',
+      user: user
+    });
+  }
 };
 
 //-------------------------------------------------------
 var demographics = function(req, res, next) {
-   if(!req.isAuthenticated()) {
-      res.redirect('/signin');
-   } else {
+  if (!req.isAuthenticated()) {
+    res.redirect('/signin');
+  } else {
 
+    var user = req.user;
 
-      var user = req.user;
+    if (user !== undefined) {
+      user = user.toJSON();
+    }
 
-      if(user !== undefined) {
-         user = user.toJSON();
-      }
+    //console.log("user: " + user.username);
 
-
-          //console.log("user: " + user.username);
-
-
-      res.render('demographics', {title: 'Demographics', user: user});
-   }
+    res.render('demographics', {
+      title: 'Demographics',
+      user: user
+    });
+  }
 };
 //------------------------------------------------------
 var demographicsPost = function(req, res, next) {
 
+  var user = req.user;
 
-      var GUID = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0,
-          v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-      });
-
-      var user = req.user;
-
-      connection.query('INSERT INTO persons (FirstName, MiddleName, LastName, Address, Zip, DOB, SSN, City, State, Country, Company, Email, Doctor, GUID)Values(' + "'" + req.body.fName + "'," + "'" + req.body.mName + "'," + "'" + req.body.lName + "'," + "'" + req.body.address + "'," + "'" + req.body.zip + "',"+ "'" + req.body.dob + "'," + "'" + req.body.ssn + "'," + "'" + req.body.city + "'," + "'" + req.body.state + "'," + "'" + req.body.country + "'," + "'" + req.body.company + "'," + "'" + req.body.email + "'," + "'" + req.body.username + "'," + "'" + GUID + "'" + ')'), function(err, rows) {
+  connection.query('INSERT INTO persons (FirstName, MiddleName, LastName, Address, Zip, DOB, SSN, City, State, Country, Company, Email, Doctor, GUID)Values(' + "'" + req.body.fName + "'," + "'" + req.body.mName + "'," + "'" + req.body.lName + "'," + "'" + req.body.address + "'," + "'" + req.body.zip + "'," + "'" + req.body.dob + "'," + "'" + req.body.ssn + "'," + "'" + req.body.city + "'," + "'" + req.body.state + "'," + "'" + req.body.country + "'," + "'" + req.body.company + "'," + "'" + req.body.email + "'," + "'" + req.body.username + "'," + "'" + GUID + "'" + ')'),
+    function(err, rows) {
       console.log("user: " + user.username);
 
 
     };
-      res.redirect('/vitals');
+
+    if (req.body.fName == "Roy") {
+        //alert("alert works")
+        res.redirect('/medication');
+      } else {
+
+  res.redirect('/vitals');
+}
 
 };
 
 //-------------------------------------------------------
-
-//-------------------------------------------------------
 var history = function(req, res, next) {
-   if(!req.isAuthenticated()) {
-      res.redirect('/signin');
-   } else {
+  if (!req.isAuthenticated()) {
+    res.redirect('/signin');
+  } else {
 
-      var user = req.user;
+    var user = req.user;
 
-      if(user !== undefined) {
-         user = user.toJSON();
-      }
-      res.render('history', {title: 'History', user: user});
-   }
+    if (user !== undefined) {
+      user = user.toJSON();
+    }
+    res.render('history', {
+      title: 'History',
+      user: user
+    });
+  }
 };
 
 //-------------------------------------------------------
 var vitals = function(req, res, next) {
-   if(!req.isAuthenticated()) {
-      res.redirect('/signin');
-   } else {
+  if (!req.isAuthenticated()) {
+    res.redirect('/signin');
+  } else {
 
-      var user = req.user;
+    var user = req.user;
 
-      if(user !== undefined) {
-         user = user.toJSON();
-      }
-      res.render('vitals', {title: 'vitals', user: user});
-   }
+    if (user !== undefined) {
+      user = user.toJSON();
+    }
+    res.render('vitals', {
+      title: 'vitals',
+      user: user,
+      GUID: GUIDReady
+    });
+  }
 };
 
 //-------------------------------------------------------
 var medication = function(req, res, next) {
-   if(!req.isAuthenticated()) {
-      res.redirect('/signin');
-   } else {
+  if (!req.isAuthenticated()) {
+    res.redirect('/signin');
+  } else {
 
-      var user = req.user;
+    var user = req.user;
 
-      if(user !== undefined) {
-         user = user.toJSON();
-      }
-      res.render('medication', {title: 'Medication', user: user});
-   }
+    if (user !== undefined) {
+      user = user.toJSON();
+    }
+    res.render('medication', {
+      title: 'Medication',
+      user: user
+    });
+  }
 };
 
 //-------------------------------------------------------
 
-
-
 var signIn = function(req, res, next) {
-   if(req.isAuthenticated()) res.redirect('/');
-   res.render('signin', {title: 'Sign In'});
+  if (req.isAuthenticated()) res.redirect('/');
+  res.render('signin', {
+    title: 'Sign In'
+  });
 
 };
 
 //-------------------------------------------------------
 
 var signInPost = function(req, res, next) {
-   passport.authenticate('local', { successRedirect: '/',
-                          failureRedirect: '/signin'}, function(err, user, info) {
-      if(err) {
-         return res.render('signin', {title: 'Sign In', errorMessage: err.message});
-      }
-
-      if(!user) {
-         return res.render('signin', {title: 'Sign In', errorMessage: info.message});
-      }
-      return req.logIn(user, function(err) {
-         if(err) {
-            return res.render('signin', {title: 'Sign In', errorMessage: err.message});
-         } else {
-            return res.redirect('/demographics');
-         }
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/signin'
+  }, function(err, user, info) {
+    if (err) {
+      return res.render('signin', {
+        title: 'Sign In',
+        errorMessage: err.message
       });
-   })(req, res, next);
+    }
+
+    if (!user) {
+      return res.render('signin', {
+        title: 'Sign In',
+        errorMessage: info.message
+      });
+    }
+    return req.logIn(user, function(err) {
+      if (err) {
+        return res.render('signin', {
+          title: 'Sign In',
+          errorMessage: err.message
+        });
+      } else {
+        return res.redirect('/demographics');
+      }
+    });
+  })(req, res, next);
 };
 
 //-----------------------------------------------------------------------------------
 
 var signUp = function(req, res, next) {
-   if(req.isAuthenticated()) {
-      res.redirect('/');
-   } else {
-      res.render('signup', {title: 'Sign Up'});
-   }
+  if (req.isAuthenticated()) {
+    res.redirect('/');
+  } else {
+    res.render('signup', {
+      title: 'Sign Up'
+    });
+  }
 };
 
 //----------------------------------------------------------------------------
 
 var signUpPost = function(req, res, next) {
-   var user = req.body;
-   var usernamePromise = null;
-   usernamePromise = new Model.User({username: user.username}).fetch();
+  var user = req.body;
+  var usernamePromise = null;
+  usernamePromise = new Model.User({
+    username: user.username
+  }).fetch();
 
-   return usernamePromise.then(function(model) {
-      if(model) {
-         res.render('signup', {title: 'signup', errorMessage: 'username already exists'});
-      } else {
-         //****************************************************//
-         // MORE VALIDATION GOES HERE(E.G. PASSWORD VALIDATION)
-         //****************************************************//
-         var password = user.password;
-         var hash = bcrypt.hashSync(password);
+  return usernamePromise.then(function(model) {
+    if (model) {
+      res.render('signup', {
+        title: 'signup',
+        errorMessage: 'username already exists'
+      });
+    } else {
+      //****************************************************//
+      // MORE VALIDATION GOES HERE(E.G. PASSWORD VALIDATION)
+      //****************************************************//
+      var password = user.password;
+      var hash = bcrypt.hashSync(password);
 
-         var signUpUser = new Model.User({username: user.username, password: hash});
+      var signUpUser = new Model.User({
+        username: user.username,
+        password: hash
+      });
 
-         signUpUser.save().then(function(model) {
-            // sign in the newly registered user
-            signInPost(req, res, next);
-         });
-      }
-   });
+      signUpUser.save().then(function(model) {
+        // sign in the newly registered user
+        signInPost(req, res, next);
+      });
+    }
+  });
 };
 
 //-------------------------------------------------------------------------------------------
 
 var signOut = function(req, res, next) {
-   // if(!req.isAuthenticated()) {
-   //    notFound404(req, res, next);
-   // } else {
-      req.logout();
-      res.redirect('/signin');
-   }
-// };
+    // if(!req.isAuthenticated()) {
+    //    notFound404(req, res, next);
+    // } else {
+    req.logout();
+    res.redirect('/signin');
+  }
+  // };
 
 //-------------------------------------------------------
 
 var notFound404 = function(req, res, next) {
-   res.status(404);
-   res.render('404', {title: '404 Not Found'});
+  res.status(404);
+  res.render('404', {
+    title: '404 Not Found'
+  });
 };
 
 //--------------------------------------------------------
