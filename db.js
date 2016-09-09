1f34df68f7ef6880a0585525814a3cec3633c233
royschuyler <royschuyler@yahoo.com>
2016-09-08 22:47:15 -0500

    corrected pw

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ var Promise = require('bluebird');
 var config = {
   host: 'us-cdbr-iron-east-04.cleardb.net',
   user: 'b92d757f64dfcb',
-  password: '8e8e5d6c ',
+  password: '8e8e5d6c',
   database: 'heroku_0a3af633b949104',
   charset: 'UTF8_GENERAL_CI'
 };
