254663f9723eed149aa68a6e2a4b8faa5aec730e
royschuyler <royschuyler@yahoo.com>
2016-09-10 14:19:18 -0500

    bs

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ passport.use(new LocalStrategy(function(username, password, done) {
    new Model.User({username: username}).fetch().then(function(data) {
       var user = data;
       if(user === null) {
-         return done(null, false, {message: 'Invalid username or password'});
+         return done(null, false, {message: 'Invalid username or password!'});
       } else {
          user = data.toJSON();
          if(!bcrypt.compareSync(password, user.password)) {
